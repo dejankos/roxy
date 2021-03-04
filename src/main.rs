3e12f492af13f40fd5302518736169b1174ca39c
dejankos <kosdejan@yahoo.com>
2021-03-04 20:17:25 +0100

    init

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use actix_web::body::Body;
+use actix_web::middleware::Logger;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
+use log::LevelFilter;
+use simplelog::{Config, TermLogger, TerminalMode};
+
+async fn hello(req: HttpRequest) -> HttpResponse {
+    println!("hello {:?}", req);
+
+    HttpResponse::Ok().message_body(Body::from("ello"))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    std::env::set_var("RUST_BACKTRACE", "1");
+    TermLogger::init(LevelFilter::Info, Config::default(), TerminalMode::Mixed).unwrap();
+
+    HttpServer::new(move || {
+        App::new()
+            .wrap(Logger::default())
+            .service(web::resource("/*").to(hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .workers(4)
+    .shutdown_timeout(10)
+    .run()
+    .await
+}
